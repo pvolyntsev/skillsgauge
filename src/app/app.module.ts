e42db8b5ca310a6306eb9f47d25f8b8f1f7be242
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2018-11-23 11:44:21 +0700

    [added] страницы 404 и about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// modules
+import { RoutingModule } from './routing/routing.module';
+
+// components
 import { AppComponent } from './app.component';
+
+// pages
+import { P404Component } from './pages/404/p404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    P404Component,
   ],
   imports: [
-    BrowserModule
+    RoutingModule,
+    BrowserModule,
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
