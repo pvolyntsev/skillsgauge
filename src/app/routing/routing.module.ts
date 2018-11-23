4a2afa5ceff0c24e73e89ec14b1ad0f100a5c5ea
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2018-11-23 11:45:48 +0700

    роутинг

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from '../pages/about/about.component';
+import { P404Component } from '../pages/404/p404.component';
+
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+
+  // { path: '/404', name: 'NotFound', component: NotFoundComponent },
+  // { path: '/*path', redirectTo: ['NotFound'] }
+  { path: '**',  component: P404Component }
+];
+
+@NgModule({
+  declarations: [
+    AboutComponent,
+  ],
+  exports: [
+    RouterModule,
+  ],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: false })
+  ]
+})
+export class RoutingModule { }
+
