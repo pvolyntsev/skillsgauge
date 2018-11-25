abfb8e9919d1475ea74233ff48b215d7c5c49882
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2018-11-26 00:29:51 +0700

    лишний закоментированный код

--- src/app/services/questionnaire-local-storage.service.ts
+++ src/app/services/questionnaire-local-storage.service.ts
@@ -5,29 +5,6 @@ import { Topic } from '../models/topic.model';
 @Injectable()
 export class QuestionnaireLocalStorageService {
 
-  // set(key: string, value: Object, ttl: number = null): void {
-  //   const now = new Date();
-  //   let expiration = null;
-  //   if (ttl !== null) {
-  //     expiration = new Date(now.getTime() + 1000 * ttl).getTime();
-  //   }
-  //   localStorage.setItem(key, JSON.stringify([value, expiration]));
-  // }
-  //
-  // get(key: string): Object {
-  //   const item = localStorage.getItem(key);
-  //   if (item !== null) {
-  //     const nowDt = new Date();
-  //     const [value, expiration] = JSON.parse(item);
-  //     const expirationDt = new Date(expiration);
-  //     if (expiration === null || expirationDt > nowDt) {
-  //       return value;
-  //     }
-  //     localStorage.removeItem(key);
-  //   }
-  //   return null;
-  // }
-
   saveTopic(topic: Topic): void {
     const today = moment().format('YYYYMMDD');
     const key = [topic.key, today].join(':');
@@ -88,4 +65,3 @@ export class QuestionnaireLocalStorageService {
     return topics;
   }
 }
-
