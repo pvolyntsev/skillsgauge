47a42aa45c704b58f20fa1b79049f91dfca38778
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2018-11-27 23:50:29 +0700

    отображение логина пользователя в верхнем меню

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription} from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { User } from '../../models';
+import { UserStore } from '../../stores';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
-
-  // selector: '[appHeader]', // tslint:disable-line
-  // template: '<ng-content></ng-content>',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSubscription: Subscription;
+  user: User;
 
-  constructor() { }
+  public visible = false;
+
+  constructor(private router: Router,
+              private userStore: UserStore) {
+
+    // обработчик на смену URL
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      console.log('HeaderComponent:constructor', event.url);
+      this.visible = event.url === '/dashboard';
+    });
+
+    // обработчик на загрузку пользователя
+    this.userSubscription = userStore.awaitUser()
+      .subscribe(
+        user => this.user = user,
+        (error) => { console.log(error); }
+      );
+  }
 
   ngOnInit() {
+  }
 
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.userSubscription.unsubscribe();
   }
 }
