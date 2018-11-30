623cdab2e640ad0ba6c3dac77d97b5de4e749e7c
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2018-12-01 00:44:37 +0700

    Информация про автора топика

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,11 +2,14 @@ export class User {
   id: string;
   login: string;
   name: string;
+  about: string;
+  home_url: string;
+  avatar_url: string;
   roles: string[] = [];
 
   public static fromObject(obj: any): User {
-    const { id, login, name, roles } = obj;
-    return Object.assign(new User(), { id, login, name, roles });
+    const { id, login, name, about, home_url, avatar_url, roles } = obj;
+    return Object.assign(new User(), { id, login, name, about, home_url, avatar_url, roles });
   }
 
   get isGuest(): Boolean {
