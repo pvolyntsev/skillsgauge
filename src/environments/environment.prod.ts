68e4ac3d457920b18f50c7b8c41addafe1093e31
Pavel Volyntsev <pavel.volyntsev@gmail.com>
2018-12-01 15:54:18 +0700

    смена хоста сервис

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  app_host: 'http://skillsgauge.uptlo.com',
-  api_host: 'http://skillsgauge.uptlo.com'
+  app_host: 'http://up.uptlo.com',
+  api_host: 'http://up.uptlo.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  app_host: 'http://skillsgauge.uptlo.com',
+  app_host: 'http://up.uptlo.com',
   api_host: ''
 };
 
